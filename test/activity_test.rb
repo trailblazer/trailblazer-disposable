b506e9e1b6b4b5e6e9f25e647812eff7b2f02a46
Nick Sutterer <apotonick@gmail.com>
2018-10-03 10:23:24 +0200

    basic processing with activities.

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -17,19 +17,30 @@ class ActivityTest < Minitest::Spec
     end
 
     step method(:recurse_scalar), id: :recurse_scalar
-    step method(:run_populator)
+    # step method(:run_populator)
 
+    # definitions.collect
     module Nested
       extend Trailblazer::Activity::Railway()
       module_function
 
       def recurse_nested(ctx, value:, dfn:, **)
-        raise (dfn[:definitions].collect do |__dfn| # TODO: that should be done via TRB's loop
-                  signal, ctx = Scalar::RunBinding.( [document: value, dfn: __dfn] )
-                  # run_binding(dfn, source, **args)
-                  ctx
-                end).inspect
-        # collect :value
+        values = dfn[:definitions].collect do |__dfn| # TODO: that should be done via TRB's loop
+
+          binding = __dfn[:binding] # Scalar::RunBinding[::Nested]
+
+          signal, (_ctx, flow) = binding.( [{document: value, dfn: __dfn}] )
+
+
+          # run_binding(dfn, source, **args)
+
+          # collect :value
+          _ctx[:value]
+        end
+
+        puts "@@@@@ #{values.inspect}"
+
+        ctx[:value] = values
       end
 
       merge!(Scalar)
@@ -66,22 +77,40 @@ class ActivityTest < Minitest::Spec
     end
   end
 
-  document = {
-    id: 1,
-    uuid: "0x11",
-    # uuid: nil,
-  }
+  it do
+    document = {
+      id: 1,
+      uuid: "0x11",
+      # uuid: nil,
+      amount: {
+        total: 9.99,
+      },
 
-  definitions = {
-    name:         :_top_,
-    definitions:  [
-      { name: :id },
-      { name: :uuid },
-    ]
-  }
+      rubbish: false,
+    }
+
+    definitions = {
+      name:         :_top_,
+
+      definitions:  [
+        { name: :id,     binding: Scalar::RunBinding },
+        { name: :uuid,   binding: Scalar::RunBinding },
+        { name: :amount, binding: Scalar::RunBinding::Nested, definitions:
+          [
+            { name: :total,   binding: Scalar::RunBinding },
+            { name: :currency, binding: Scalar::RunBinding },
+          ]
+        }
+      ]
+    }
 
 
-  pp Scalar::RunBinding.( [document: document, dfn: definitions[:definitions][0]] )
+    pp Scalar::RunBinding.( [document: document, dfn: definitions[:definitions][0]] )
 
-  pp Scalar::RunBinding::Nested.( [document: {_top_: document}, dfn: definitions] )
+    signal, (ctx, _) = Scalar::RunBinding::Nested.( [document: {_top_: document}, dfn: definitions] )
+
+    pp signal, ctx
+
+    ctx[:value].must_equal([:_top_, [[:id, 1], [:uuid, "0x11"], [:amount, [[:total, 9.99], nil]]]])
+  end
 end
